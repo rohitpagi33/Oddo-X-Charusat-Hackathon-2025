0c5ff443e0fbfd7c31b66e560446b1c270608d99
Prince61141 <princepatel61141@gmail.com>
2025-03-01 17:46:53 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,6 @@ import LoginPage from "./pages/LoginPage";
 import Dashboard_User from "./pages/Dashboard_User";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import Chatbot from "./components/AI/AI";
-
-// Admin Components
-// import Analytics from "./components/Admin/Analytics";
-import { Overview } from "./components/Admin/overview";
-import Borrowers from "./components/Admin/Borrower";
-import { LoanAnalytics } from "./components/Admin/loan-analytics";
-import { PaymentAnalytics } from "./components/Admin/payment-analytics";
-import { RecentApplications } from "./components/Admin/recent-applications";
-// import RiskAssessment from "./components/Admin/RiskAssessment";
-import Settings from "./components/Dashboard/Settings";
-import Sidebar from "./components/Dashboard/Sidebar";
-// import AdminLayout from "./components/Admin/AdminLayout"; // 
 
 function App() {
   return (
@@ -31,21 +18,6 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard_user" element={<Dashboard_User />} />
         <Route path="/dashboard_agency" element={<Dashboard_Agency />} />
-        {/* <Route path="/AI" element={<Chatbot />} /> */}
-
-        {/* Admin Routes */}
-        {/* <Route path="/admin" element={<AdminLayout />}> */}
-          {/* <Route path="analytics" element={<Analytics />} /> */}
-          <Route path="/admin/overview" element={<Overview />} />
-          <Route path="/admin/borrowers" element={<Borrowers />} />
-          <Route path="/admin/loan-analytics" element={<LoanAnalytics />} />
-          <Route path="/admin/payment-analytics" element={<PaymentAnalytics />} />
-          <Route path="/admin/recent-applications" element={<RecentApplications />} />
-          {/* <Route path="/admin/risk-assessment" element={<RiskAssessment />} /> */}
-          <Route path="/admin/settings" element={<Settings />} />
-          <Route path="/admin/*" element={<h1>Not Found</h1>} />
-          <Route path="/admin/sidebar" element={<Sidebar />} />
-        
       </Routes>
     </Router>
   );
