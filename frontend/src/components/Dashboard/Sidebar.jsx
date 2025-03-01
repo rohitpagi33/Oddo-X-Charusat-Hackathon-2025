0b61e895945f1c3e8a14ed9e0cd2713da4363877
Harshils411 <harshilsolanki114@gmail.com>
2025-03-01 19:36:21 +0530

    Enhance Sidebar component with react-icons for improved visual clarity and user experience

--- frontend/src/components/Dashboard/Sidebar.jsx
+++ frontend/src/components/Dashboard/Sidebar.jsx
@@ -1,4 +1,11 @@
+import { RiNumbersFill } from "react-icons/ri"; 
+import { MdSchedule } from "react-icons/md"; 
 import React from "react";
+import { MdOutlineDashboard } from "react-icons/md";
+import { TiDocumentText } from "react-icons/ti";
+import { FiMessageSquare } from "react-icons/fi";
+import { IoDocumentTextOutline } from "react-icons/io";
+import { CiSettings } from "react-icons/ci";
 
 const Sidebar = ({ setActiveComponent }) => {
   return (
@@ -10,7 +17,8 @@ const Sidebar = ({ setActiveComponent }) => {
             className="btn btn-dark w-100 text-start"
             onClick={() => setActiveComponent("dashboard")}
           >
-            📊 Dashboard
+                         <MdOutlineDashboard />  Dashboard
+             
           </button>
         </li>
         <li>
@@ -18,23 +26,23 @@ const Sidebar = ({ setActiveComponent }) => {
             className="btn btn-dark w-100 text-start"
             onClick={() => setActiveComponent("apply_loan")}
           >
-            💰 Apply Loan
+           <TiDocumentText />  Loan Apply
           </button>
         </li>
-        <li>
+        {/* <li>
           <button
             className="btn btn-dark w-100 text-start"
             onClick={() => setActiveComponent("settings")}
           >
             ⚙️ Settings
           </button>
-        </li>
+        </li> */}
         <li>
           <button
             className="btn btn-dark w-100 text-start"
             onClick={() => setActiveComponent("emi")}
           >
-            EMI Schedual
+            <MdSchedule /> EMI Schedual
           </button>
         </li>
         <li>
@@ -42,7 +50,7 @@ const Sidebar = ({ setActiveComponent }) => {
             className="btn btn-dark w-100 text-start"
             onClick={() => setActiveComponent("cibil")}
           >
-            CIBIL Score
+           <RiNumbersFill /> CIBIL Score
           </button>
         </li>
         <li>
@@ -50,7 +58,7 @@ const Sidebar = ({ setActiveComponent }) => {
             className="btn btn-dark w-100 text-start"
             onClick={() => setActiveComponent("messages")}
           >
-            Messages 
+             <FiMessageSquare />  Messages 
           </button>
         </li>
       </ul>
